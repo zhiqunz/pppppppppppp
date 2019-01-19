8c07ce0f18c17056267d573a0683bec3f7043981
zhuzhiqun <bgt55tgb>
2019-01-20 00:53:24 +0800

    update base file

--- base.py
+++ base.py
@@ -3,7 +3,7 @@ import os
 
 class Base:
     def __init__(self, target, fun_get, fun_process, all_flg=False):
-        print('** new obj: {}, all flg: {} **'.format(target, all_flg))
+        print('*** new obj: {}, all flg: {} ***'.format(target, all_flg))
         self.lines_list = []
         self.files_name_list = []
         self.files_path_list = []
@@ -12,48 +12,65 @@ class Base:
         if os.access(target, os.F_OK):
             if not os.access(target, os.R_OK):
                 print('access is NOT allowed: {}'.format(target))
-                raise 'access is NOT allowed: {}'.format(target)
+                raise BaseException('access is NOT allowed: {}'.format(target))
         else:
             print('Not exist: {}'.format(target))
-            raise 'Not exist: {}'.format(target)
+            raise BaseException('Not exist: {}'.format(target))
 
         if not hasattr(fun_get, '__call__'):
             print('fun_get must be call: {}'.format(fun_get))
+            raise BaseException('fun_get must be call: {}'.format(fun_get))
 
         if not hasattr(fun_process, '__call__'):
             print('fun_process must be call: {}'.format(fun_process))
+            raise BaseException('fun_process must be call: '
+                                '{}' .format(fun_process))
 
         self.target = target
         self.fun_get = fun_get
         self.fun_process = fun_process
         self.all_flg = all_flg
 
-        # 如果处理参数是文件夹，获取该文件夹内的所有文件；
-        # 如果参数all_flg为true，递归获取该文件夹和子文件夹内所有文件
+        # 如果target参数是文件夹，获取该文件夹内的所有文件；
+        # 如果all_flg参数为true，获取该文件夹和子文件夹内所有文件
+        # 如果target参数是文件，获取该文件的所有行
+        if os.path.isdir(target):
+            if all_flg:
+                dir_result = os.walk(target)
+                for entry in dir_result:
+                    for name in entry[2]:
+                        self.files_name_list.append(name)
+                        self.files_path_list.append(os.path.join(entry[0],
+                                                                 name))
+            else:
+                dir_result = os.scandir(target)
+                for entry in dir_result:
+                    self.files_name_list.append(entry.name)
+                    self.files_path_list.append(entry.path)
+        elif os.path.isfile(target):
+            with open(target, 'r', encoding="utf-8") as f:
+                for line in f:
+                    self.lines_list.append(line)
+                    # line = line.replace(os.linesep, '')
+                    # if line:
+                    #     self.lines_list.append(line)
+        else:
+            raise BaseException('target must be folder or file: '
+                                '{}'.format(target))
 
-        # 如果处理参数是文件，获取该文件的所有行
-
-    def get_folder(self, folder_path=''):
+    def get_target(self):
         pass
 
-    def get_file(self, file_path=''):
-        pass
-
-    def get_target(self, func=''):
-        pass
-
-    def process_target(self, func=''):
+    def process_target(self, process_obj):
         pass
 
     def run(self):
-        print('**start run {}**'.format(self.target))
-        if self.fun_get and \
-                hasattr(self.fun_get, '__call__') and \
-                self.fun_process and \
-                hasattr(self.fun_process, '__call__'):
+        print('*** start run {} ***'.format(self.target))
+        if self.fun_get and hasattr(self.fun_get, '__call__') and \
+           self.fun_process and hasattr(self.fun_process, '__call__'):
             run_result = self.fun_get()
             self.fun_process(run_result)
-        print('**end run {}**'.format(self.target))
+        print('*** end   run {} ***'.format(self.target))
 
 
 if __name__ == '__main__':
@@ -61,10 +78,11 @@ if __name__ == '__main__':
     test_file_path = os.path.join(cp, 'c0.py')
     test_folder_path = os.path.join(cp, 'templates')
 
-    def func01():
+    def func01(*args, **kwargs):
         pass
 
-    def func02():
+    def func02(*args, **kwargs):
         pass
 
-    test_obj = Base(test_folder_path, func01, func02)
+    test_obj = Base(test_file_path, func01, func02)
+    test_obj.run()
